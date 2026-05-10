d6023d724566bd8ccb16e11225450eb8d3b3de14
Elias <ekasa123@gmail.com>
2026-05-10 14:06:09 +0300

    delete all

--- assets/js/security.js
+++
@@ -1,9 +0,0 @@
-const tempDiv = document.createElement("div");
-
-export function sanitizeInput(text) {
-  if (!text) return "";
-  tempDiv.textContent = text;
-  const sanitized = tempDiv.innerHTML;
-  tempDiv.textContent = "";
-  return sanitized;
-}
